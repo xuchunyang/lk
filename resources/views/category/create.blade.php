16899c530c08e0c177ddf679aa2800a0b815bf5d
Xu Chunyang <xuchunyang56@gmail.com>
2022-11-09 11:34:03 +0800

    Polish category edit/create page

--- resources/views/category/create.blade.php
+++ resources/views/category/create.blade.php
@@ -1,15 +1,17 @@
-<x-layout title="Create Category">
+<x-layout title="创建分类">
     <div class="container mx-auto p-4">
-        <h1 class="mb-4 font-medium text-lg">Create Category</h1>
-        <form action="{{ route('categories.store') }}" method="post" enctype="multipart/form-data">
-            @csrf
-            <x-form.input name="name" help="分类名称，如 Arduino" required/>
-            <x-form.input name="slug" required help="match /[a-z-]+/"/>
-            <x-form.input name="title" help="分类标题，如 Arduino 中文社区" required/>
-            <x-form.input name="logo" type="image" help="image/*, 建议比例 1:1, max 200KB" required/>
-            <x-form.input name="color" type="color" help="分类主题色" default="{{ fake()->hexColor() }}"/>
-            <x-form.input name="description" type="markdown" help="分类一句话介绍" required/>
-            <x-form.submit/>
-        </form>
+        <div class="bg-white rounded shadow p-4">
+            <h1 class="mb-4 font-medium text-lg">创建分类</h1>
+            <form action="{{ route('categories.store') }}" method="post" enctype="multipart/form-data">
+                @csrf
+                <x-form.input name="name" help="分类名称，如 Arduino" required/>
+                <x-form.input name="slug" required help="match /[a-z-]+/"/>
+                <x-form.input name="title" help="分类标题，如 Arduino 中文社区" required/>
+                <x-form.input name="logo" type="image" help="image/*, 建议比例 1:1, max 200KB" required/>
+                <x-form.input name="color" type="color" help="分类主题色" default="{{ fake()->hexColor() }}"/>
+                <x-form.input name="description" type="markdown" help="分类一句话介绍" required/>
+                <x-form.submit/>
+            </form>
+        </div>
     </div>
 </x-layout>
